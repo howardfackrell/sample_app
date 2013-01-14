0bc36dc7adfebc1d21c9a33e3eef2cf3f0e8f843
Howard Fackrell <hfackrell@gmail.com>
2013-01-13 21:22:32 -0700

    Add static contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,17 @@ describe "Static pages" do
                                 :text => " | About")
     end
   end
+  
+  describe "Contact page" do
+    it "should have the h1 'Contact Us" do
+      visit '/static_pages/contact' do
+       page.should have_selector('h1', text: "Contact") 
+      end
+    end
+    
+    it "should have the title 'Contanct Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: " | Contact")
+    end
+  end
 end
