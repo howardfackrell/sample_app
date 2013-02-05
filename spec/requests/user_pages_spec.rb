9eda68a4ab9f39088bdff538e083330c14231de9
Howard Fackrell <hfackrell@gmail.com>
2013-02-04 21:23:59 -0700

    add a sign up page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@ describe "UserPages" do
   subject { page }
   describe "GET /user_pages" do
     before { visit signup_path }
-    it { should have_selector( 'h1', text: 'Sign up' ) }
+    it { should have_selector( 'h1', text: 'Sign Up' ) }
     it { should have_selector( 'title', text: full_title('Sign up' )) }
   end
 end
